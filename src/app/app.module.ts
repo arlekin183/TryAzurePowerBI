1c791b458806f754298d4fc9fc5764a6fc3d6eca
Anton Alekseev <Anton_Alekseev1@epam.com>
2022-02-22 09:11:34 +0400

    Initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,112 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { IPublicClientApplication, PublicClientApplication, InteractionType, BrowserCacheLocation, LogLevel, ProtocolMode } from '@azure/msal-browser';
+import { MsalGuard, MsalInterceptor, MsalBroadcastService, MsalInterceptorConfiguration, MsalModule, MsalService, MSAL_GUARD_CONFIG, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalGuardConfiguration, MsalRedirectComponent } from '@azure/msal-angular';
+import { EmbeddedComponent } from './embedded/embedded.component';
+import { PowerBIEmbedModule } from 'powerbi-client-angular';
+
+const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
+
+export function loggerCallback(logLevel: LogLevel, message: string) {
+  console.log(message);
+}
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      // clientId: 'c9196ba0-fd85-4d76-8c0b-705bdcae6c36',
+      clientId: '973dcaa1-e10b-400a-99c5-4df6fe451400',
+      authority: 'https://login.microsoftonline.com/b41b72d0-4e9f-4c26-8a69-f949f367c91d/',
+      redirectUri: 'http://localhost:4201/embedded/',      
+      // clientId: 'Enter_the_Application_Id_Here',
+      // authority: 'Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here',
+      // redirectUri: 'Enter_the_Redirect_Uri_Here'
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+      storeAuthStateInCookie: isIE, // set to true for IE 11
+    },
+    system: {
+      loggerOptions: {
+        loggerCallback,
+        logLevel: LogLevel.Info,
+        piiLoggingEnabled: false
+      }
+    }
+  });
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ['user.read']);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap
+  };
+}
+
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return { 
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: ['user.read']
+    }
+  };
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    EmbeddedComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
+    HttpClientModule,
+    MsalModule,
+    PowerBIEmbedModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService
+  ],
+  bootstrap: [AppComponent, MsalRedirectComponent]
+})
+export class AppModule { }
---
+++ src/app/embedded/embedded.component.ts
@@ -0,0 +1,78 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { IReportEmbedConfiguration, models, Page, Report, service, VisualDescriptor } from 'powerbi-client';
+import { PowerBIReportEmbedComponent } from 'powerbi-client-angular';
+import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+
+@Component({
+  selector: 'app-embedded',
+  templateUrl: './embedded.component.html',
+  styleUrls: ['./embedded.component.css']
+})
+export class EmbeddedComponent {
+  @ViewChild(PowerBIReportEmbedComponent) reportObj!: PowerBIReportEmbedComponent;
+
+  reportConfig: IReportEmbedConfiguration = {
+    type: "report",
+    id: 'fa077a48-c124-4be2-b54d-ff0e9d3596f4', //<Report Id>
+    embedUrl: 'https://app.powerbi.com/reportEmbed?reportId=fa077a48-c124-4be2-b54d-ff0e9d3596f4&groupId=6fcbe016-8581-4d4d-becc-5f2025efb0b1',
+    tokenType: models.TokenType.Embed,
+    accessToken: '', // Keep as empty string, null or undefined
+    // hostname: "https://app.powerbi.com"
+}
+  accessToken = '';
+  reportClass = 'report-container hidden';
+  isEmbedded = false;
+  isBIReportShowed=false;
+  eventHandlersMap = new Map<string, (event?: service.ICustomEvent<any>) => void>([
+    ['loaded', () => console.log('Report has loaded')],
+    [
+      'rendered',
+      () => {
+        console.log('Report has rendered');
+
+        // Set displayMessage to empty when rendered for the first time
+        if (!this.isEmbedded) {
+        }
+
+        // Update embed status
+        this.isEmbedded = true;
+      },
+    ],
+    [
+      'error',
+      (event?: service.ICustomEvent<any>) => {
+        if (event) {
+          console.error(event.detail);
+        }
+      },
+    ],
+    ['visualClicked', () => console.log('visual clicked')],
+    ['pageChanged', (event) => console.log(event)],
+  ]);
+
+  constructor(
+    private element: ElementRef<HTMLDivElement>,
+    private authService: MsalService,
+    private msalBroadcastService: MsalBroadcastService,
+  ) { 
+    this.accessToken = <string>localStorage.getItem('accessToken');
+    this.reportConfig = {
+      ...this.reportConfig,
+      // embedUrl: 'https://embedded.powerbi.com/appTokenReportEmbed?reportId=fa077a48-c124-4be2-b54d-ff0e9d3596f4',
+      accessToken: this.accessToken,
+    }
+  }  
+
+  showBIReport(){
+    this.isBIReportShowed=!this.isBIReportShowed;
+    const reportDiv = this.element.nativeElement.querySelector('.report-container');
+    if (reportDiv) {
+      if(this.isBIReportShowed) {
+        reportDiv.classList.remove('hidden');
+      } else {
+        reportDiv.classList.add('hidden');
+      }
+    }
+  }
+}
